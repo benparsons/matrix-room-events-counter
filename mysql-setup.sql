a5fb290541329da5c74a29f7d69026a7887e9e54
Ben Parsons <ben@bpulse.co.uk>
2018-09-27 15:17:38 +0100

    typo

--- mysql-setup.sql
+++ mysql-setup.sql
@@ -4,7 +4,7 @@ CREATE TABLE advocacy_room_health (
     room_id	varchar(100),
     canonical_alias	varchar(100),
     message_count	INTEGER,
-    sneder_count	INTEGER
+    sender_count	INTEGER
 );
 ALTER TABLE advocacy_room_health ADD PRIMARY KEY(ds, room_id);
 GRANT SELECT ON businessmetrics.advocacy_room_health TO 'grafanauser'@'localhost';
