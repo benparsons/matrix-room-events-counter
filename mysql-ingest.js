39572d4a2aba332f919027fe3b8704791a97fdbe
Ben Parsons <ben@bpulse.co.uk>
2018-09-27 15:13:45 +0100

    handle slow db

--- mysql-ingest.js
+++ mysql-ingest.js
@@ -1,40 +1,43 @@
 var config = require('../project-health-github/config.json');
 var mysql = require('mysql');
 var mysqlConn = mysql.createConnection(config.mysql);
+const parse = require('csv-parse')
+const fs = require('fs');
 mysqlConn.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
+    readCsv();
 });
-const parse = require('csv-parse')
-const fs = require('fs');
-var files = fs.readdirSync("data/");
-console.log(files);
-files.forEach(file => {
-    var data = fs.readFileSync("data/" + file, 'utf-8');
-    parse(data.replace(/\'/g, "\""), {columns:true}, function(err, rows){
-        console.log(rows);
-        var rowsHandled = 0; 
-        rows.forEach(row => {
-            var sql = `
-                REPLACE INTO advocacy_room_health (
-                    ds, room_id, canonical_alias, message_count, sender_count
-                )
-                VALUES (
-                    '${row.ds}',
-                    '${row.room_id}',
-                    '${row.canonical_alias}',
-                    ${row.message_count},
-                    ${row.sender_count}
-                )
-            `;
-            console.log(sql);
-            mysqlConn.query(sql, function() {
-                rowsHandled++;
-                if (rowsHandled === rows.length) {
-                    process.exit(0);
-                }
+function readCsv() {
+    var files = fs.readdirSync("data/");
+    console.log(files);
+    files.forEach(file => {
+        var data = fs.readFileSync("data/" + file, 'utf-8');
+        parse(data.replace(/\'/g, "\""), {columns:true}, function(err, rows){
+            console.log(rows);
+            var rowsHandled = 0; 
+            rows.forEach(row => {
+                var sql = `
+                    REPLACE INTO advocacy_room_health (
+                        ds, room_id, canonical_alias, message_count, sender_count
+                    )
+                    VALUES (
+                        '${row.ds}',
+                        '${row.room_id}',
+                        '${row.canonical_alias}',
+                        ${row.message_count},
+                        ${row.sender_count}
+                    )
+                `;
+                console.log(sql);
+                mysqlConn.query(sql, function() {
+                    rowsHandled++;
+                    if (rowsHandled === rows.length) {
+                        process.exit(0);
+                    }
+                });
             });
+                    
         });
-                
     });
-});
+}
